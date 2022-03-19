f074faa74256be4a2a18d867ad7948280e33e11d
Ricardo Gayer <ricardo.r.gayer@gmail.com>
2022-03-19 19:43:49 -0300

    color menu

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     "./layouts/**/*.{html,js,jsx,md,mdx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [require('@tailwindcss/forms')],
 }
